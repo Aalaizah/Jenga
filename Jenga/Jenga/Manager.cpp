7302b64c6238bb8c867e42bda1b08b798648b03d
Andrew "AJ" Mandula <ajman@csh.rit.edu>
2014-04-27 16:45:57 -0400

    added a movable cube in a 3D space
    based off code from http://www.wikihow.com/Make-a-Cube-in-OpenGL

--- Jenga/Jenga/Manager.cpp
+++ Jenga/Jenga/Manager.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 int window;
 
+double rotate_y=0; 
+double rotate_x=0;
+
 Manager::Manager(void)
 {
 }
@@ -25,14 +28,89 @@ void idle (void)
 void display(void)
 {
 	glutSetWindow(window);  // main window
-
+	
 // clear all pixels in the buffer
-    glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+	//Resets transformations
+	glLoadIdentity();
+
+	// Rotate when user changes rotate_x and rotate_y
+  glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+  glRotatef( rotate_y, 0.0, 1.0, 0.0 );
 
 //  drawing code goes here
-	
+	glBegin(GL_POLYGON);
+
+	// White side - BACK
+glBegin(GL_POLYGON);
+glColor3f(   1.0,  1.0, 1.0 );
+glVertex3f(  0.5, -0.5, 0.5 );
+glVertex3f(  0.5,  0.5, 0.5 );
+glVertex3f( -0.5,  0.5, 0.5 );
+glVertex3f( -0.5, -0.5, 0.5 );
+glEnd();
+ 
+// Purple side - RIGHT
+glBegin(GL_POLYGON);
+glColor3f(  1.0,  0.0,  1.0 );
+glVertex3f( 0.5, -0.5, -0.5 );
+glVertex3f( 0.5,  0.5, -0.5 );
+glVertex3f( 0.5,  0.5,  0.5 );
+glVertex3f( 0.5, -0.5,  0.5 );
+glEnd();
+ 
+// Green side - LEFT
+glBegin(GL_POLYGON);
+glColor3f(   0.0,  1.0,  0.0 );
+glVertex3f( -0.5, -0.5,  0.5 );
+glVertex3f( -0.5,  0.5,  0.5 );
+glVertex3f( -0.5,  0.5, -0.5 );
+glVertex3f( -0.5, -0.5, -0.5 );
+glEnd();
+ 
+// Blue side - TOP
+glBegin(GL_POLYGON);
+glColor3f(   0.0,  0.0,  1.0 );
+glVertex3f(  0.5,  0.5,  0.5 );
+glVertex3f(  0.5,  0.5, -0.5 );
+glVertex3f( -0.5,  0.5, -0.5 );
+glVertex3f( -0.5,  0.5,  0.5 );
+glEnd();
+ 
+// Red side - BOTTOM
+glBegin(GL_POLYGON);
+glColor3f(   1.0,  0.0,  0.0 );
+glVertex3f(  0.5, -0.5, -0.5 );
+glVertex3f(  0.5, -0.5,  0.5 );
+glVertex3f( -0.5, -0.5,  0.5 );
+glVertex3f( -0.5, -0.5, -0.5 );
+
+
+	glEnd();
 //  start processing buffered OpenGL routines 
     glutSwapBuffers();
+}
+
+void specialKeys( int key, int x, int y ) {
+ 
+  //  Right arrow - increase rotation by 5 degree
+  if (key == GLUT_KEY_RIGHT)
+    rotate_y += 5;
+ 
+  //  Left arrow - decrease rotation by 5 degree
+  else if (key == GLUT_KEY_LEFT)
+    rotate_y -= 5;
+ 
+  else if (key == GLUT_KEY_UP)
+    rotate_x += 5;
+ 
+  else if (key == GLUT_KEY_DOWN)
+    rotate_x -= 5;
+ 
+  //  Request display update
+  glutPostRedisplay();
+ 
 }
 
 void init (void) 
@@ -45,7 +123,7 @@ void init (void)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 // orthographic projection
-    glOrtho(-200.0, 200.0, -200.0, 200.0, -200.0, 200.0);
+    glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
 }
 
 /* 
@@ -73,14 +151,17 @@ int main(int argc, char** argv)
 	// create window with given title displayed
     window = glutCreateWindow ("Jenga");  // change string for a new title
 
+	glEnable(GL_DEPTH_TEST);
+
 	// our initialization function from above
     init();
 
 	// callback for display - gets called whenever window needs to be redisplayed
     glutDisplayFunc(display); 
-
-	// additional callbacks
 	
+	// additional callbacks
+	glutSpecialFunc(specialKeys);
+
 	glutIdleFunc(idle); // call if nothing else is going on
 
 	// event handling loop - keeps program from exiting after
